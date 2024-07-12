1a2d19baec04b6cc55883091b84977fea5496102
copperfork <137697221+copperfork@users.noreply.github.com>
2024-07-12 22:45:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let damn;
-damn = 1;
-
-console.log(damn);
+function confirmData(){
+    const plaintext = document.getElementById(cipherEntry);
+    const plaintextGet = cipherEntry.value.toUpperCase();
+    console.log("Unprocessed Input: " +plaintextGet);
+    const characters = plaintextGet.split("");
+    console.log(characters.charCodeAt(""));
+    // The "key part"
+    const key = document.getElementById(keyInput);
+    const keyGet = keyInput.value;
+    console.log("Unprocessed Input: " +keyGet);
+    const keyProcessed = keyGet.split("");
+    console.log(keyProcessed.length);
+    // Shifting
+    if(characters.length > keyProcessed.length){
+       const indexed = characters.length % keyProcessed.length
+       console.log('shift: ' +indexed)
+    }
+}
